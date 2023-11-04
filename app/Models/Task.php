e5de3daeba9533558eb7b1ca127bd1b6c2d36411
robertvitoriano <robertvitoriano@gmail.com>
2023-11-04 22:01:45 +0900

    add add routes for creating, listing and showing tasks

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
+    
+    protected $fillable = [
+        'title',
+    ];
+    protected $casts = [
+        'is_done' => 'boolean'
+    ];
+    
+    protected $hidden = [
+        'updated_at',
+    ];
 }

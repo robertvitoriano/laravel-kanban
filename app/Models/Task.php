09d946b2d5d66ac49b125ab86f06edc0e4242261
robertvitoriano <robertvitoriano@gmail.com>
2023-11-05 22:20:18 +0900

    enable authorization middleware to tasks routes and creates user with task relationship

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Facades\Auth;
 
 class Task extends Model
 {
@@ -13,6 +15,7 @@ class Task extends Model
     protected $fillable = [
         'title',
         'is_done',
+        'creator_id',
     ];
     protected $casts = [
         'is_done' => 'boolean'
@@ -21,5 +24,12 @@ class Task extends Model
     public function creator():BelongsTo
     {
         return $this->belongsTo(User::class, 'creator_id');
+    }
+    
+    protected static function booted(): void 
+    {
+        static::addGlobalScope('creator', function(Builder $builder){
+           $builder->where('creator_id', Auth::id());
+        });
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -15,17 +15,13 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::post('login',[AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('tasks', TaskController::class)->only([
-    'index',
-    'show',
-    'store',
-    'update',
-    "destroy",
-]);
+Route::middleware('auth:sanctum')->group(function(){
+    Route::apiResource('tasks', TaskController::class);    
+});
 
-Route::post('login',[AuthController::class, 'login']);

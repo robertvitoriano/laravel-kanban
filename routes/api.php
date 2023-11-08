b3d5aee19495b5234e15d6316e99469a4749c6d5
robertvitoriano <robertvitoriano@gmail.com>
2023-11-08 21:15:24 +0900

    refactor: change name of authcontroller to usercontroller

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\TaskController;
 use Illuminate\Http\Request;
@@ -16,16 +16,14 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-Route::post('login',[AuthController::class, 'login']);
-Route::post('register',[AuthController::class, 'register']);
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::post('login',[UserController::class, 'login']);
+Route::post('register',[UserController::class, 'register']);
+Route::middleware('User:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::middleware('auth:sanctum')->group(function(){
+Route::middleware('User:sanctum')->group(function(){
     Route::apiResource('tasks', TaskController::class);   
     Route::apiResource('projects', ProjectController::class);
-    Route::delete('user',[AuthController::class, 'destroy']);
-
 });
 

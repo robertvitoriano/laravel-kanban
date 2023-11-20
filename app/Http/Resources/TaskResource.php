ebe07d941de048dc9d6d1f27e3bf4b5c4992fcaa
robertvitoriano <robertvitoriano@gmail.com>
2023-11-21 08:20:47 +0900

    adjust list heights based on cards quantity

--- app/Http/Resources/TaskResource.php
+++ app/Http/Resources/TaskResource.php
@@ -15,6 +15,7 @@ class TaskResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            "id" => $this->id,
             "title" => $this->title,
             "done" => $this->is_done,
             "status" =>  $this->is_done ? 'finished' : 'open',

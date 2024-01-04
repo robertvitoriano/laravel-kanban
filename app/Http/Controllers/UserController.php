8b86862004449eb07d1d8cf6125dce90c61a27fe
Robert Vitoriano <robertvitorianodev@gmail.com>
2024-01-04 17:35:41 +0900

    add data key to response

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -81,6 +81,7 @@ class UserController extends Controller
 
         return response()->json([
             'message' =>  'Successfully updated!',
+            'data' => $validated,
         ], 201);
     }
 

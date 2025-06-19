f85a8d306ec1698c7a8959fc1c089a6d77b365e0
Robert Vitoriano <robertvitorianodev@gmail.com>
2025-06-19 13:40:48 -0300

    run composer install

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
+if [ ! -f /var/www/vendor/autoload.php ]; then
+    echo "vendor/autoload.php not found. Running composer install..."
+    composer install --no-interaction --prefer-dist --optimize-autoloader
+fi
+
 echo "Run migrations"
 
 echo "MySQL is ready. Running migrations..."
